6aca26bc97e1348a56221ad7c4686301850594f1
quickglobe <enzoscot@gmail.com>
2026-06-02 18:10:44 +0100

    Use quickglobe-avatar.svg as favicon; simplify header logo to img tag

--- .github/scripts/gen-icons.py
+++ .github/scripts/gen-icons.py
@@ -2,7 +2,7 @@ import cairosvg
 from PIL import Image
 import io
 
-SVG = 'favicon.svg'
+SVG = 'quickglobe-avatar.svg'
 BG  = '#f4efe4'
 
 def render(size):
@@ -14,21 +14,17 @@ def flatten(img, bg=BG):
     base.paste(img, mask=img.split()[3])
     return base
 
-# Favicon PNGs
 for size in [16, 32, 48]:
     render(size).save(f'favicon-{size}x{size}.png')
     print(f'favicon-{size}x{size}.png')
 
-# apple-touch-icon: 180x180, corners flattened onto solid bg
 flatten(render(180)).save('apple-touch-icon.png')
 print('apple-touch-icon.png')
 
-# Android Chrome
 for size in [192, 512]:
     render(size).save(f'android-chrome-{size}x{size}.png')
     print(f'android-chrome-{size}x{size}.png')
 
-# favicon.ico: multi-resolution
 render(256).save('favicon.ico', sizes=[(16, 16), (32, 32), (48, 48)])
 print('favicon.ico')
 
